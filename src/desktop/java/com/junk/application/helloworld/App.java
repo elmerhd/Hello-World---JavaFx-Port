977d641d443ece7c1257d1f0f3980005e831762c
Elmer Duron <elmerduron2016@gmail.com>
2019-06-24 16:09:06 +0800

    update content size from screen
    update content size from screen

--- src/desktop/java/com/junk/application/helloworld/App.java
+++ src/desktop/java/com/junk/application/helloworld/App.java
@@ -2,8 +2,10 @@ package com.junk.application.helloworld;
 
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 
 /**
  *
@@ -16,9 +18,9 @@ public class App extends Application {
     public void start(Stage primaryStage) throws IOException {
         
         Main main = Main.getInstance("/com/junk/application/helloworld/fxml/main.fxml");
-        
-        Scene scene = new Scene(main.getContent(), 300, 250);
-        primaryStage.setMaximized(true);
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+
+        Scene scene = new Scene(main.getContent(), primaryScreenBounds.getWidth(), primaryScreenBounds.getHeight());
         primaryStage.setScene(scene);
         primaryStage.setTitle("Hello World - JavaFx Port");
         primaryStage.show();
